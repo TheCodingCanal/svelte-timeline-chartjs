b82be9014a9fc152aa03b5bf5eb76946ec14e644
The Coding Canal <thecodingcanal@gmail.com>
2024-04-13 06:24:25 -0500

    first commit

---
+++ src/components/Chart.svelte
@@ -0,0 +1,32 @@
+<script>
+	export const ssr = false;
+	export const csr = true;
+
+	import { Bar } from 'svelte-chartjs';
+	import { data } from '../lib/data.js';
+	import 'chartjs-adapter-date-fns';
+	import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+	import {
+		Chart,
+		TimeScale,
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale
+	} from 'chart.js';
+
+	Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, TimeScale);
+</script>
+
+<Bar
+	{data}
+	options={{
+		indexAxis: 'y',
+		responsive: true,
+		scales: { x: { type: 'time', time: { unit: 'day' }, min: '2022-10-01', max: '2022-10-15' } }
+	}}
+	plugins={[ChartDataLabels]}
+/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Chart from '../components/Chart.svelte';
+</script>
+
+<main>
+	<Chart />
+</main>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+</style>

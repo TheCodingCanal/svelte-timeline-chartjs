6949b663c65b25d877b6b5d2a52dd892928b4947
Sophia Seely <sophiaseely@outlook.com>
2024-05-09 15:03:58 -0500

    wip: construction: Tooltip flips to the left when in the right most half of the screen. Tooltip is missing background when it flips.

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -2,16 +2,6 @@
 	export const ssr = false;
 	export const csr = true;
 
-	import {
-		BarElement,
-		CategoryScale,
-		Chart,
-		Legend,
-		TimeScale,
-		Title,
-		Tooltip,
-		type TooltipModel
-	} from 'chart.js';
 	import 'chartjs-adapter-date-fns';
 	import ChartDataLabels from 'chartjs-plugin-datalabels';
 	import { Bar } from 'svelte-chartjs';
@@ -22,23 +12,35 @@
 	let tooltipLeft: number = 0;
 	let tooltipTop: number = 0;
 	let tooltipBottom: number = 0;
+	let tooltipRight: number = 0;
 	let tooltipOpacity: number = 0;
 
-	const minDateStr = '2022-01-01';
-	const maxDateStr = '2022-12-31';
-	// const minDate = new Date(minDateStr);
-	// const maxDate = new Date(maxDateStr);
-	// const data = generateData(minDate, maxDate);
+	const minDateStr = '2022-10-01';
+	const maxDateStr = '2022-10-15';
+
+	import {
+		BarElement,
+		CategoryScale,
+		Chart,
+		Legend,
+		TimeScale,
+		Title,
+		Tooltip,
+		type TooltipModel
+	} from 'chart.js';
 
 	Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, TimeScale);
 
 	function externalTooltipHandler(
-		this: TooltipModel<'bar'>,
-		context: { chart: Chart; tooltip: TooltipModel<'bar'> }
-	): void {
-		const { chart, tooltip } = context;
+			this: TooltipModel<'bar'>,
+			context: { chart: Chart; tooltip: TooltipModel<'bar'> }
+	): void  {
 
-		if (tooltip.opacity === 0) {
+		const {chart, tooltip} = context;
+
+
+
+		if (tooltip.opacity === 0){
 			tooltipOpacity = 0;
 			return;
 		}
@@ -47,39 +49,48 @@
 		console.log('bar data', tooltip.$context.tooltipItems[0].element); // bar width should be what we want to center
 		console.log(context);
 
+		tooltipDataIndex = tooltip.$context.tooltipItems[0].dataIndex;
+		tooltipDatasetIndex = tooltip.$context.tooltipItems[0].datasetIndex;
+
 		tooltipOpacity = 1;
 		tooltipLeft = tooltip.caretX;
 		tooltipTop = tooltip.caretY;
 		tooltipBottom = 0;
+		tooltipRight = 0;
+		console.log(tooltip);
 
-		console.log('tooltip.caretY', tooltip.caretY);
-
-		console.log('bottom', chart.chartArea.bottom);
 		if (tooltip.caretY > chart.chartArea.bottom / 2) {
 			tooltipTop = 0;
 			tooltipBottom = chart.chartArea.bottom - tooltip.caretY;
 		}
+		if(tooltip.caretX > chart.chartArea.right / 2){
+			tooltipRight =  tooltip.caretX;
+			// tooltipLeft = tooltip.caretX;
+		}
 	}
 </script>
 
-<div style="position: relative;">
-	<Bar
-		{data}
-		options={{
-			indexAxis: 'y',
-			responsive: true,
-			scales: { x: { type: 'time', time: { unit: 'day' }, min: minDateStr, max: maxDateStr } },
-			plugins: {
-				tooltip: {
-					enabled: false,
-					position: 'nearest',
-					external: externalTooltipHandler
-				}
-			}
-		}}
-		plugins={[ChartDataLabels]}
-	/>
-	<TooltipText
+<Bar
+	{data}
+	options={{
+		indexAxis: 'y',
+		responsive: true,
+		scales: { x: { type: 'time', time: { unit: 'day' }, min: minDateStr, max: maxDateStr } },
+    plugins: {
+        tooltip: {
+			enabled: false,
+			position: 'nearest',
+            external: externalTooltipHandler
+
+        }
+
+    }
+	}}
+
+	plugins={[ChartDataLabels]}
+
+/>
+<TooltipText
 		{data}
 		indexB={tooltipDataIndex}
 		indexA={tooltipDatasetIndex}
@@ -87,5 +98,6 @@
 		top={tooltipTop}
 		bottom={tooltipBottom}
 		opacity={tooltipOpacity}
-	></TooltipText>
-</div>
+		right={tooltipRight}
+></TooltipText>
+
--- src/components/TooltipText.svelte
+++ src/components/TooltipText.svelte
@@ -1,65 +1,78 @@
 <script lang="ts">
 	import type { ChartData } from '$lib/types';
 
-	export let data: ChartData;
+import type { ChartData } from '$lib/types';
+export let data: ChartData;
+export let indexA: number = 0;
+export let indexB: number = 0;
+export let left = 0;
+export let top = 0;
+export let bottom = 0;
+export let right = 0;
+export let opacity = 0;
 
-	export let indexA = 0;
-	export let indexB = 0;
-	export let left = 0;
-	export let top = 0;
-	export let bottom = 0;
-	export let opacity = 0;
+let style: string;
 
-	let style: string;
-	$: {
-		style = `left: ${left}px; opacity: ${opacity};`;
-		if (bottom !== 0) {
-			style += `bottom: ${bottom}px;`;
-		} else {
-			style += `top: ${top}px;`;
-		}
-	}
+
+$: {
+    style = `left: ${left}px; opacity: ${opacity};`;
+    // style = ` opacity: ${opacity};`;
+    if (bottom !== 0) {
+        style += `bottom: ${bottom}px;`;
+    }
+    if(bottom == 0) {
+        style += `top: ${top}px;`;
+    }
+    if(right !== 0){
+        style += `right: ${right}px;`;
+        style += 'display: flex; justify-content: flex-end;'
+        console.log('right', right);
+    }
+    // if(right == 0){
+    //     style += `left: ${left}px;`;
+    //     console.log(left);
+    // }
+}
 
 	$: console.log({ top, bottom, opacity, style });
 </script>
 
-<div class="tooltip" {style}>
-	<table>
-		<tbody>
-			{#each data.datasets[indexA].data[indexB].tooltip as content}
-				<tr>
-					{#each Object.values(content) as cell}
-						<td>{cell}</td>
-					{/each}
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+<div class="tooltip" id="tooltip" {style}>
+    <table>
+        <tbody>
+            {#each data.datasets[indexA].data[indexB].tooltip as content}
+                <tr>
+                    {#each Object.values(content) as cell}
+                        <td>{cell}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
 </div>
 
 <style>
-	table {
-		color: black;
-	}
-	tr:nth-child(even) {
-		background-color: whitesmoke;
-	}
-	tr:nth-child(odd) {
-		background-color: darkgray;
-	}
-	td {
-		padding-left: 15px;
-		padding-right: 15px;
-	}
+    table {
+        color: black;
+    }
+    tr:nth-child(even) {
+        background-color: whitesmoke;
+    }
+    tr:nth-child(odd) {
+        background-color: darkgray;
+    }
+    td {
+        padding-left: 15px;
+        padding-right: 15px;
+    }
 
-	.tooltip {
-		position: absolute;
-		background-color: rgba(0, 0, 0, 0.7);
-		border-radius: 3px;
-		color: white;
-		pointer-events: none;
-		position: absolute;
-		transform: translate(-100%, 0);
-		transition: all 0.1s ease;
-	}
+    .tooltip {
+        position: absolute;
+        background-color: rgba(0, 0, 0, 0.7);
+        border-radius: 3px;
+        color: white;
+        pointer-events: none;
+        transform: translate(-0%, 0);
+        transition: all 0.1s ease;
+    }
 </style>

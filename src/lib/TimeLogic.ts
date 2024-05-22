961c5a8b4f53c684a4b3c03c877b95fe2a7eb518
Sophia Seely <sophiaseely@outlook.com>
2024-05-22 13:23:29 -0500

    wip/tests pass. tests cover each posibility for the function XAxisAdjustment.

--- src/lib/TimeLogic.ts
+++ src/lib/TimeLogic.ts
@@ -1,47 +1,47 @@
 // import {TimeData} from "$lib/TimeData";
 import type {DateTime, XAxisTime} from "$lib/types";
 
+const XAxis: XAxisTime = {};
 
-
-function differenceBettweenDates(timeInfo: DateTime){
+function differenceBettweenDates(timeInfo: DateTime) {
     const timeMax: Date = timeInfo.max;
     const timeMin: Date = timeInfo.min;
     const timeDifference: number = timeMax.getTime() - timeMin.getTime();
     console.log(timeInfo.min);
     console.log(timeMax.getTime() - timeMin.getTime());
-    const timeDifferenceHours: number =timeDifference / (60000 * 60);
+    const timeDifferenceHours: number = timeDifference / (60000 * 60);
     return timeDifferenceHours;
 }
-const XAxis: XAxisTime = {};
-export function XAxisAdjustment(timeInfo: DateTime): XAxisTime{
 
-   const timespan: number = differenceBettweenDates(timeInfo);
+export function XAxisAdjustment(timeInfo: DateTime): XAxisTime {
 
-   XAxis.min = timeInfo.min;
-   XAxis.max = timeInfo.max;
+    const timespan: number = differenceBettweenDates(timeInfo);
 
-   if(timespan < 2){
-       XAxis.unit = "minute";
-       XAxis.stepSize = 15;
-       return XAxis;
-   }
-   if (timespan < 4 && timespan > 2){
-       XAxis.unit = "minute";
-       XAxis.stepSize = 30;
-       return XAxis;
-   }
-   if (timespan > 4 && timespan < 24){
-       XAxis.unit = "hour";
-       XAxis.stepSize = 1;
-       return XAxis;
-   }
-   if(timespan > 24){
-       XAxis.unit = "day";
-       XAxis.stepSize = 1;
-       return XAxis;
-   }
-   XAxis.unit = "day";
-   XAxis.stepSize = 1;
-   return XAxis;
-   console.log(timespan)
+    XAxis.min = timeInfo.min;
+    XAxis.max = timeInfo.max;
+
+    if (timespan < 2) {
+        XAxis.unit = "minute";
+        XAxis.stepSize = 15;
+        return XAxis;
+    }
+    if (timespan < 4 && timespan > 2) {
+        XAxis.unit = "minute";
+        XAxis.stepSize = 30;
+        return XAxis;
+    }
+    if (timespan > 4 && timespan < 24) {
+        XAxis.unit = "hour";
+        XAxis.stepSize = 1;
+        return XAxis;
+    }
+    if (timespan > 24) {
+        XAxis.unit = "day";
+        XAxis.stepSize = 1;
+        return XAxis;
+    }
+    XAxis.unit = "day";
+    XAxis.stepSize = 1;
+    return XAxis;
+    console.log(timespan)
 }

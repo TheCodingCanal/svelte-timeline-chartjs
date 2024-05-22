13c180f43f7a45e043e132200c74b3daac10b5eb
Sophia Seely <sophiaseely@outlook.com>
2024-05-22 12:04:46 -0500

    wip/ test runs but does not pass. XAxis.max is invalid date.

--- src/lib/TimeData.ts
+++ src/lib/TimeData.ts
@@ -2,13 +2,13 @@ import type {DateTime, XAxisTime} from "$lib/types";
 
 export const TimeData:XAxisTime ={
     unit: "minute",
-    min: "2022-10-01T12:00",
-    max: "2022-10-01T15:00",
+    min: new Date("2022-10-01T12:00"),
+    max: new Date("2022-10-01T15:00"),
     stepSize: 15
 
 }
 
-export const DateTime: DateTime ={
+export const DatedTime: DateTime ={
     min: new Date("2022-10-01T12:00"),
-    max: new Date("2022-10-01T18:00"),
+    max: new Date("2022-10-01T18:00")
 }

3f6571ae1be34c1c3401e9114d4faac0507c2f48
Sophia Seely <sophiaseely@outlook.com>
2024-06-05 15:26:37 -0500

    wip/ tooltip shortens for the width of the bar but does not take into account how much of the bar is on the screen.

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -11,6 +11,7 @@
     import TooltipText from './TooltipText.svelte';
     import {XAxisAdjustment} from "$lib/TimeLogic";
     import type {XAxisTime} from "$lib/types";
+    import {characterWidthEstimates, formatText} from "$lib/dataLabelTruncator";
 
     let tooltipDataIndex: number = 0;
     let tooltipDatasetIndex: number = 0;
@@ -81,11 +82,18 @@
         }
     }
 
-    function updateLabel(chart: Chart){
+    function updateLabel( chart: Chart,){
+
+        characterWidthEstimates();
         if(chart) {
             for(let datasetIndex: number = 0; datasetIndex < chart.data.datasets.length; datasetIndex++){
                 for(let dataIndex: number = 0; dataIndex < chart.data.datasets[datasetIndex].data.length; dataIndex++){
                     // chart.data.datasets[datasetIndex].data[dataIndex].label = "this is a test";
+                    if(chart.data.datasets[datasetIndex].data[dataIndex].label) {
+                        let dataLabelString = chart.data.datasets[datasetIndex].data[dataIndex].label;
+                        const barWidth: number = chart.getDatasetMeta(datasetIndex).data[dataIndex].width;
+                        chart.data.datasets[datasetIndex].data[dataIndex].label = formatText(dataLabelString, barWidth);
+                    }
                     
                 }
             }
--- src/lib/dataLabelTruncator.ts
+++ src/lib/dataLabelTruncator.ts
@@ -1,3 +1,56 @@
-function TextWidth (text: string){
+const textWidthCache: Record<string, number> ={};
 
+export function formatText(text: string, barWidth: number) :string{
+    const textByLine: string[] = text.toString().split('/n');
+
+    let result: string = "";
+    for(const line of textByLine)
+   {
+       let totalSize: number = 0;
+       for(const character of line) {
+           if ((totalSize += textWidth(character)) > barWidth) {
+               break;
+           }
+           result += character;
+       }
+           result += "\n"
+       }
+    return result;
+}
+function textWidth (text: string): number{
+    let total = 0;
+    if(text && text !== ''){
+       const textArray: string[] = text.split("");
+       textArray.forEach(
+           function(char: string){
+               total += charWidth(char);
+           }
+       )
+    }
+    return total;
+}
+
+function charWidth (char: string) :number{
+    if (textWidthCache[char]){
+        return textWidthCache[char];
+    }
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    if(context) {
+
+        const width = context.measureText(char).width;
+        textWidthCache[char] = width;
+    }
+    return textWidthCache[char];
+}
+
+export function characterWidthEstimates() {
+    const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-/:%';
+    const characterWidths: Record<string, number> = {};
+
+    for (const character of characters) {
+        characterWidths[character] = charWidth(character);
+    }
+
+    return characterWidths;
 }
